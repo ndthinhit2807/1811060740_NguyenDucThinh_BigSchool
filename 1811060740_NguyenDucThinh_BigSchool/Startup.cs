83dc739257dde1c200ecd662237a8c8d0c19ba7a
ndthinhit2807 <75533943+ndthinhit2807@users.noreply.github.com>
2021-06-30 13:42:25 +0700

    Add project files.

---
+++ 1811060740_NguyenDucThinh_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1811060740_NguyenDucThinh_BigSchool.Startup))]
+namespace _1811060740_NguyenDucThinh_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
