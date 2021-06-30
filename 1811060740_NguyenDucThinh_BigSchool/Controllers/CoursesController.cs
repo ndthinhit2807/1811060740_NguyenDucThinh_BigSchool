27e5b996ac68866e170fcd2a48bc2adc36ddccfd
ndthinhit2807 <75533943+ndthinhit2807@users.noreply.github.com>
2021-06-30 14:58:44 +0700

    ok

---
+++ 1811060740_NguyenDucThinh_BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,29 @@
+﻿using _1811060740_NguyenDucThinh_BigSchool.Models;
+using _1811060740_NguyenDucThinh_BigSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _1811060740_NguyenDucThinh_BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+        
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
